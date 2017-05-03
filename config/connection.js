13b6e2b53a0fd665de8fdee6542574f1d5b0591c
Ricky Grams <rickygrams@Rickys-MacBook-Pro.local>
2017-05-02 22:41:51 -0700

    final stages

--- config/connection.js
+++ config/connection.js
@@ -23,8 +23,3 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
-
-
-
-
-
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,4 +8,45 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    // console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/create", function(req, res) {
+  burger.create([
+    "burger_name"
+  ], [
+    req.body.burger_name,
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  // console.log(req.params.id);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
+
+
+
+
+
